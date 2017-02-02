d85642b2030d41ee0d318829618816087f6a4ce8
hannay1 <hannay11211@live.com>
2017-02-01 23:38:50 -0700

    f

--- mitm_buffer.py
+++ mitm_buffer.py
@@ -18,7 +18,7 @@ class MITM_Interface:
 		self.initTableB()
 		self.initTableS()
 
-	#hashmaps for use when sending to debriefing page
+	#hashmaps for use when sending to control pannel
 	def tableA_vals(self, user_id, pword_id, associated_account, password_length, strength):
 		return {"user_id" : user_id,
 		"pword_id" : pword_id,
@@ -26,42 +26,35 @@ class MITM_Interface:
 		"password_length" : password_length,
 		"strength" : strength}
 
-	def tableB_vals(self,user_id, number_shared_passwords):
+	def tableB_vals(self,user_id, password_id, number_shared_passwords, accounts):
 		return {"user_id" : user_id,
-		"number_shared_passwords" : number_shared_passwords}
+		"password_id" : password_id,
+		"number_shared_passwords" : number_shared_passwords,
+		"accounts" : accounts}
 
 	def assign_strength(self, password):
 		return self.analyzer.score_password(password)
 
-	def check_for_shared_passwords(self, uid):
-		try:
-			query = 'CREATE TABLE IF NOT EXISTS TableA' \
-					'(user_id TEXT PRIMARY KEY NOT NULL,' \
-					'pword_id TEXT UNIQUE NOT NULL,' \
-					'associated_account TEXT NOT NULL,' \
-					'strength INTEGER NOT NULL)'
-			self.cur.execute(query)
-			self.connex.commit()
-		except sqlite3.Error as SQE:
-			print("error inserting record:", SQE)
-			pass
 
 	def read_traffic(self, account, password):
 		print "mitm_buffer.py uid: " + self.current_user
 		p_id = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(12))
 		strength = str(self.assign_strength(password))
 		length = str(len(password))
-		number_shared_passwords = self.get_user_pwords_accounts(self.current_user)
 		self.insert_into_table_S(self.current_user, account, password)
+		number_shared_passwords = self.get_user_pwords_accounts(self.current_user)
+		associated_accounts = number_shared_passwords["accounts"]
+		num_passwords =  number_shared_passwords['number_shared_passwords']
+		password_id = number_shared_passwords['password_id']
 		self.insert_into_table_A(self.current_user, p_id, account, length, strength)
-		self.insert_into_table_B(self.current_user, number_shared_passwords)
+		self.insert_into_table_B(self.current_user, password_id, num_passwords, associated_accounts)
 		pass
 
 
 	def initTableA(self):
 		try:
 			query = 'CREATE TABLE IF NOT EXISTS TableA' \
-					'(user_id TEXT PRIMARY KEY NOT NULL,' \
+					'(user_id TEXT NOT NULL,' \
 					'pword_id TEXT UNIQUE NOT NULL,' \
 					'associated_account TEXT NOT NULL,' \
 					'password_length TEXT NOT NULL,' \
@@ -76,7 +69,9 @@ class MITM_Interface:
 		try:
 			query = 'CREATE TABLE IF NOT EXISTS TableB' \
 					'(user_id TEXT PRIMARY KEY NOT NULL,' \
-					'number_shared_passwords INTEGER NOT NULL)'
+					'password_id TEXT NOT NULL,' \
+					'number_shared_passwords INTEGER NOT NULL,' \
+					'shared_accounts TEXT NOT NULL)'
 			self.cur.execute(query)
 			self.connex.commit()
 		except sqlite3.Error as SQE:
@@ -95,18 +90,29 @@ class MITM_Interface:
 		except sqlite3.Error as SQE:
 			print("error inserting record:", SQE)
 			pass
+#Fzh6zn6R
+
+
+
 
 	def get_user_pwords_accounts(self, u_id):
+		paswds = []
 		try:
-			self.cur.execute("SELECT password, associated_account FROM TableS WHERE user_id = (?)", [u_id])
-			self.cur.execute("SELECT password, associated_account, COUNT() FROM TableS WHERE user_id = (?)", [u_id])
-			pw_db = self.cur.fetchall()
-			for row in pw_db:
-				print str(row)
-				#get all passwords from account a, b, etc...
-			if not pw_db:
-				raise sqlite3.Error
-			return pw_db
+			self.cur.execute("SELECT DISTINCT password from TableS WHERE user_id = (?)", [u_id])
+			passwords = self.cur.fetchall()
+			i = 0
+			for row in passwords:
+				i+=1
+				self.cur.execute("SELECT COUNT(password) FROM TableS WHERE password = (?) and user_id = (?)", [row[0], u_id] )
+				counts = self.cur.fetchall()
+				count = counts[0][0]
+				self.cur.execute("SELECT associated_account FROM TableS WHERE password = (?) and user_id = (?)", [row[0], u_id] )
+				accounts = self.cur.fetchall()
+				account_list = self.get_words(accounts)
+				print "password #" + str(i) + " : " + str(count) + " accounts:" + str(accounts)
+				paswds.append(("Password # " + str(i), count, account_list))
+			vals = self.tableB_vals(self.current_user, paswds[0][0], paswds[0][1], paswds[0][2])
+			return vals
 		except sqlite3.Error as SQE:
 			print("error updating record:", SQE)
 			return None
@@ -115,18 +121,18 @@ class MITM_Interface:
 	def insert_into_table_A(self, user_id, pword_id, associated_account, password_length, strength):
 		try:
 			print("adding to table a...")
-			self.cur.execute("INSERT OR IGNORE INTO TableA VALUES (?,?,?,?,?)", [user_id, pword_id, associated_account, password_length, strength])
+			self.cur.execute("INSERT INTO TableA VALUES (?,?,?,?,?)", [user_id, pword_id, associated_account, password_length, strength])
 			self.connex.commit()
 		except sqlite3.Error as SQE:
 			print("error inserting record:", SQE)
 			pass
 
-	def insert_into_table_B(self, user_id, number_shared_passwords):
+	def insert_into_table_B(self, user_id, password_id, number_shared_passwords, shared_accounts):
 		try:
 			print("adding to table b...")
-			self.cur.execute("INSERT OR IGNORE INTO TableB VALUES (?,?)", [user_id, number_shared_passwords])
+			self.cur.execute("INSERT or REPLACE INTO TableB VALUES (?,?,?,?)", [user_id, password_id, number_shared_passwords, shared_accounts])
 			self.connex.commit()
-			print("successfully inserted records into tables...")
+			print("successfully inserted records into tables... BBBBBBBB")
 		except sqlite3.Error as SQE:
 			print("error inserting record:", SQE)
 			pass
@@ -134,6 +140,7 @@ class MITM_Interface:
 
 	def insert_into_table_S(self, user_id, associated_account, password):
 		password = self.hash_pword(password)
+		password = base64.b64encode(password)
 		table_s = str(user_id)
 		try:
 			print("adding into secure table...")
